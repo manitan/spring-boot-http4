6df0638e609079d876fa0cfc34ce7d6ad6c01726
manitan <atsushi.imai3@gmail.com>
2018-10-26 19:04:44 +0900

    add date field

--- src/main/java/hello/Greeting.java
+++ src/main/java/hello/Greeting.java
@@ -4,10 +4,12 @@ public class Greeting {
 
     private final long id;
     private final String content;
+    private final Date date; // 追加
 
     public Greeting(long id, String content) {
         this.id = id;
         this.content = content;
+        this.date = new Date(); // 追加
     }
 
     public long getId() {
@@ -18,4 +20,8 @@ public class Greeting {
         return content;
     }
 
+    // このメソッドを追加
+    public Date getDate() {
+        return date;
+    }
 }
